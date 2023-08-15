f15f2eb95a900ca80f3791545ff429008f9793da
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-08-15 09:44:19 +0200

    add eventListener

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 //Fetch Data
 async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  );
   const data = await response.json();
   const characters = data.results;
+  maxPage = data.info.pages;
   cardContainer.innerHTML = "";
   characters.forEach((character) => {
     const newCard = createCharacterCard(character);
@@ -27,3 +30,19 @@ async function fetchCharacters() {
   });
 }
 fetchCharacters();
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+  }
+  // else if (page === 1) {
+  //   prevButton.getAttribute("disabled");
+  // }
+  // prevButtone.disabled = true;
+});
+
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+  }
+});
